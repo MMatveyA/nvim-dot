c0a1ed87c994d0533bd4c0d0b476093f44e7f7bd
Maximov Matvey Alexeevich <maksimovmatvey2005@gmail.com>
2024-01-10 13:24:44 +0400

    Добавлена настройка nvim-dap для python

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,5 +1,6 @@
 local dap = require("dap")
 
+-- ADAPTERS --
 dap.adapters.codelldb = {
 	type = "server",
 	port = "${port}",
@@ -9,6 +10,30 @@ dap.adapters.codelldb = {
 	},
 }
 
+dap.adapters.debugpy = function(cb, config)
+	if config.request == "attach" then
+		local port = (config.connect or config).port
+		local host = (config.connect or config).host or "127.0.0.1"
+		cb({
+			type = "server",
+			port = assert(port, "`connect.port` is required for a python `attach` configuration"),
+			host = host,
+			options = {
+				source_filetypes = "python",
+			},
+		})
+	else
+		cb({
+			type = "executable",
+			command = "/home/matvey/.local/share/nvim/mason/bin/debugpy",
+			options = {
+				source_filetypes = "python",
+			},
+		})
+	end
+end
+
+-- CONFIGURATIONS --
 dap.configurations.cpp = {
 	{
 		name = "Launch file",
@@ -19,5 +44,25 @@ dap.configurations.cpp = {
 		end,
 		cwd = "${workspaceFolder}",
 		stopOnEntry = false,
+	},
+}
+
+dap.configurations.python = {
+	{
+		type = "python",
+		request = "launch",
+		name = "Launch file",
+
+		program = "${file}",
+		pythonPath = function()
+			local cwd = vim.fn.getcwd()
+			if vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
+				return cwd .. "/venv/bin/python"
+			elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
+				return cwd .. "/.venv/bin/python"
+			else
+				return "/usr/bin/python"
+			end
+		end,
 	},
 }
